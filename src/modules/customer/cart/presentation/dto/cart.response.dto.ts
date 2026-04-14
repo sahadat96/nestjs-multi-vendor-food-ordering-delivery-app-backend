3bbceec261712fb8dff702ac005b3fae90cbe434
sahadat96 <sahadatappdev@gmail.com>
2026-04-14 12:30:39 +0600

    Cart response dto creted

---
+++ src/modules/customer/cart/presentation/dto/cart.response.dto.ts
@@ -0,0 +1,24 @@
+export class CartItemResponseDto {
+  id!: string;
+
+  productId!: string;
+  name!: string;
+
+  quantity!: number;
+  price!: number;
+
+  sizeOption?: {
+    id: string;
+    name: string;
+  };
+
+  choiceOptions?: {
+    id: string;
+    name: string;
+  }[];
+
+  addOns?: {
+    id: string;
+    name: string;
+  }[];
+}
