2bd9c38c86d94813f378b06ceefbacaaa6ff0829
sahadat96 <sahadatappdev@gmail.com>
2026-05-04 12:20:27 +0600

    vendor/active-order created this method in order controller

--- src/modules/order/presentation/controllers/order.controller.ts
+++ src/modules/order/presentation/controllers/order.controller.ts
@@ -8,17 +8,18 @@ import {
   Patch,
 } from '@nestjs/common';
 
+import { CreateOrderDto } from '../dto/create-order.dto';
 import { 
   OrderSummaryResponseDto,
   OrderTrackResponseDto,
+  CreateOrderResponseDto,
+  VendorActiveOrdersResponseDto,
 } from '../dto/order.response.dto';
 
 import { CurrentUser } from '@/modules/auth/decorators/get-user.decorator';
 import type { AuthUser } from '@/modules/auth/domain/interfaces/auth-user.interface';
 import { ResponseMessage } from '@/common/decorators/response-message.decorator';
 import { OrderService } from '../../application/order.service';
-import { CreateOrderDto } from '../dto/create-order.dto';
-import { CreateOrderResponseDto } from '../dto/order.response.dto';
 import { RoleGuard } from 'src/common/guards/roles.guard';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { Role } from 'src/common/enums/role.enum';
@@ -69,4 +70,13 @@ export class OrderController {
   ): Promise<OrderTrackResponseDto> {
     return this.orderService.userCancelOrder(user.id, orderId);
   }
+
+@Get('vendor/active-order')
+@UseGuards(RoleGuard)
+@Roles(Role.VENDOR)
+async getVendorActiveOrders(
+  @CurrentUser() user: AuthUser,
+): Promise<VendorActiveOrdersResponseDto> {
+  return this.orderService.getVendorActiveOrders(user.id);
+}
 }
