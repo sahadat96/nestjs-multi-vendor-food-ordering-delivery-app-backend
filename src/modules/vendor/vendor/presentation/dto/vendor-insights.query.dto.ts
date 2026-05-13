dd0a9d69742d004bcdb3c12ee4490c1817769077
sahadat96 <sahadatappdev@gmail.com>
2026-05-13 11:37:55 +0600

    this class added in dto vendor query dto

--- src/modules/vendor/vendor/presentation/dto/vendor-insights.query.dto.ts
+++ src/modules/vendor/vendor/presentation/dto/vendor-insights.query.dto.ts
@@ -1,7 +1,13 @@
 
-import { IsOptional, IsString, Matches, IsIn} from 'class-validator';
+import { IsOptional, IsIn} from 'class-validator';
 
 export class VendorInsightsOverviewQueryDto {
+  @IsOptional()
+  @IsIn(['today', 'week', 'month', 'year'])
+  range?: 'today' | 'week' | 'month' | 'year' = 'month';
+}
+
+export class VendorAiGuidanceQueryDto {
   @IsOptional()
   @IsIn(['today', 'week', 'month', 'year'])
   range?: 'today' | 'week' | 'month' | 'year' = 'month';
